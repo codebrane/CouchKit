860028726089c8e05fd108b066e9f93800bc656a
Alistair Young <alistair@codebrane.com>
2009-11-04 16:40:44 +0000

    Now tests adding a document via raw JSON

--- src/test/java/com/codebrane/couchdb/AddDocumentTest.java
+++ src/test/java/com/codebrane/couchdb/AddDocumentTest.java
@@ -12,14 +12,22 @@ import static junit.framework.Assert.fail;
 public class AddDocumentTest extends CouchDBTest {
   @Test
   public void test() {
-    System.out.println("AddDocumentTest");
     CouchPotato cp = new CouchPotato(couchDBServer);
     cp.connect();
     try {
+      // Add a domain object
+      System.out.println("AddDocumentTest : domain object");
       CouchPotatoResult cpResult = cp.addDocument(TEST_DB_NAME, TEST_DOC_ID, cpTestDoc);
       Assert.assertNotNull(cpResult);
       Assert.assertEquals("true", cpResult.ok);
       Assert.assertEquals("111", cpResult.id);
+
+      // Add a raw JSON object
+      System.out.println("AddDocumentTest : JSON");
+      CouchPotatoResult cpJSONResult = cp.addDocument(TEST_DB_NAME, TEST_JSON_DOC_ID, testJSON);
+      Assert.assertNotNull(cpJSONResult);
+      Assert.assertEquals("true", cpJSONResult.ok);
+      Assert.assertEquals(TEST_JSON_DOC_ID, cpJSONResult.id);
     }
     catch(CouchPotatoException cpe) {
       fail(cpe.getMessage());
