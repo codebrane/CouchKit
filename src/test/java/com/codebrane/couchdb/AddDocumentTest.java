8304815f47ad69b0e9bb41c3599b03b1cac5fe07
Alistair Young <alistair@codebrane.com>
2009-11-04 15:52:43 +0000

    Now handles CouchPotatoException

--- src/test/java/com/codebrane/couchdb/AddDocumentTest.java
+++ src/test/java/com/codebrane/couchdb/AddDocumentTest.java
@@ -2,6 +2,7 @@ package com.codebrane.couchdb;
 
 import org.junit.Test;
 import org.junit.Assert;
+import static junit.framework.Assert.fail;
 
 /**
  * Test that adds a document to a CouchDB database
@@ -12,12 +13,19 @@ public class AddDocumentTest extends CouchDBTest {
   @Test
   public void test() {
     System.out.println("AddDocumentTest");
-    CouchPotato cp = new CouchPotato(props.getString(PROP_COUCHDBSERVER));
+    CouchPotato cp = new CouchPotato(couchDBServer);
     cp.connect();
-    CouchPotatoResult cpResult = cp.addDocument(TEST_DB_NAME, TEST_DOC_ID, cpTestDoc);
-    Assert.assertNotNull(cpResult);
-    Assert.assertEquals("true", cpResult.ok);
-    Assert.assertEquals("111", cpResult.id);
-    cp.disconnect();
+    try {
+      CouchPotatoResult cpResult = cp.addDocument(TEST_DB_NAME, TEST_DOC_ID, cpTestDoc);
+      Assert.assertNotNull(cpResult);
+      Assert.assertEquals("true", cpResult.ok);
+      Assert.assertEquals("111", cpResult.id);
+    }
+    catch(CouchPotatoException cpe) {
+      fail(cpe.getMessage());
+    }
+    finally {
+      cp.disconnect();
+    }
   }
 }
