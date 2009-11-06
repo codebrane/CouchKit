201ab4a617764d07e89fecc9363c0d10812dc201
Alistair Young <alistair@codebrane.com>
2009-11-06 11:20:01 +0000

    CouchPotato -> CouchKit

---
+++ src/main/java/com/codebrane/couchdb/CouchKitResult.java
@@ -0,0 +1,12 @@
+package com.codebrane.couchdb;
+
+/**
+ * Represents a general result from a CouchDB operation
+ *
+ * @author alistair
+ */
+public class CouchKitResult {
+  public String ok = null;
+  public String id = null;
+  public String rev = null;
+}
